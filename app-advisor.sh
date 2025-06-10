fb34562542af1ca3c685581e1fb1b39f88657a6c
DaShaun Carter <dashaun@dashaun.com>
2025-06-10 18:49:47 -0500

    remove server requirement, change version

--- app-advisor.sh
+++ app-advisor.sh
@@ -1,9 +1,9 @@
-VERSION=1.1.2
+VERSION=1.3.1
 curl -L -H "Authorization: Bearer $ARTIFACTORY_TOKEN" -o /tmp/advisor-cli.tar -X GET https://packages.broadcom.com/artifactory/spring-enterprise/com/vmware/tanzu/spring/application-advisor-cli-linux/$VERSION/application-advisor-cli-linux-$VERSION.tar
 tar -xf /tmp/advisor-cli.tar --strip-components=1 -C /tmp
 install /tmp/advisor /usr/local/bin/advisor
 
 advisor build-config get
-advisor build-config publish --url=${APP_ADVISOR_SERVER}
-advisor upgrade-plan get --url=${APP_ADVISOR_SERVER}
-advisor upgrade-plan apply --push --url=${APP_ADVISOR_SERVER}
+#advisor build-config publish --url=${APP_ADVISOR_SERVER}
+advisor upgrade-plan get
+advisor upgrade-plan apply --push
